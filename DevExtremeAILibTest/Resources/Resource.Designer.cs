2db7adcdff560341a52805b9ee2adb029ded1979
AndreaPic <andrea.piccioni@libero.it>
2023-05-19 11:15:09 +0200

    rename

--- DevExtremeAILibTest/Resources/Resource.Designer.cs
+++ DevExtremeAILibTest/Resources/Resource.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace DevExtremeAILibTestX.Resources {
+namespace DevExtremeAILibTest.Resources {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace DevExtremeAILibTestX.Resources {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DevExtremeAILibTestX.Resources.Resource", typeof(Resource).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DevExtremeAILibTest.Resources.Resource", typeof(Resource).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
