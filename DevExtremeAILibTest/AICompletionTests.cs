95f8b4930860dd2ccf7a8f45da7df56f40ca7eaf
AndreaPic <andrea.piccioni@libero.it>
2023-06-04 10:00:24 +0200

    added support for multiple stops in completions

--- DevExtremeAILibTest/AICompletionTests.cs
+++ DevExtremeAILibTest/AICompletionTests.cs
@@ -48,6 +48,73 @@ namespace DevExtremeAILibTest
         [Theory]
         [InlineData("text-davinci-003")]
         //[InlineData("gpt-3.5-turbo")]
+        public async Task CreateCompletionArrayTest(string modelID)
+        {
+
+            //await Task.Delay(22000);
+            using (var scope = _factory.Services.CreateScope())
+            {
+                var openAiapiClient = scope.ServiceProvider.GetService<IOpenAIAPIClient>();
+                CreateCompletionRequest createCompletionRequest = new CreateCompletionRequest();
+                createCompletionRequest.Model = modelID;
+                createCompletionRequest.MaxTokens = 1024;
+                createCompletionRequest.Temperature = 0.3F;
+                createCompletionRequest.Stream = false;
+                createCompletionRequest.LogProbs = null;
+
+                string prompt = "\nQUESTION: What is the Italian's capital?";
+
+                createCompletionRequest.AddCompletionPrompt(prompt);
+
+                var completionResponse = await openAiapiClient.CreateCompletionAsync(createCompletionRequest);
+                Assert.False(completionResponse.HasError, completionResponse?.ErrorResponse?.Error?.Message);
+                Assert.NotNull(completionResponse?.OpenAIResponse);
+                Assert.NotNull(completionResponse?.OpenAIResponse?.Choices);
+                Assert.True(completionResponse.OpenAIResponse.Choices.Count > 0);
+                Assert.NotNull(completionResponse?.OpenAIResponse?.Usage);
+
+                prompt += completionResponse.OpenAIResponse.Choices[0].Text;
+                prompt += "\nQUESTION: " + "what can i see there?";
+
+                createCompletionRequest = new CreateCompletionRequest();
+                createCompletionRequest.Model = modelID;
+                createCompletionRequest.MaxTokens = 1024;
+                createCompletionRequest.Temperature = 0.3F;
+                createCompletionRequest.Stream = false;
+                createCompletionRequest.LogProbs = null;
+                createCompletionRequest.AddCompletionPrompt(prompt);
+
+                completionResponse = await openAiapiClient.CreateCompletionAsync(createCompletionRequest);
+                Assert.False(completionResponse.HasError, completionResponse?.ErrorResponse?.Error?.Message);
+                Assert.NotNull(completionResponse?.OpenAIResponse);
+                Assert.NotNull(completionResponse?.OpenAIResponse?.Choices);
+                Assert.True(completionResponse.OpenAIResponse.Choices.Count > 0);
+                Assert.NotNull(completionResponse?.OpenAIResponse?.Usage);
+
+                prompt += completionResponse.OpenAIResponse.Choices[0].Text;
+                prompt += "\nQUESTION: " + "what is the best?";
+                createCompletionRequest = new CreateCompletionRequest();
+                createCompletionRequest.Model = modelID;
+                createCompletionRequest.MaxTokens = 1024;
+                createCompletionRequest.Temperature = 0.3F;
+                createCompletionRequest.Stream = false;
+                createCompletionRequest.LogProbs = null;
+                createCompletionRequest.AddCompletionPrompt(prompt);
+
+                completionResponse = await openAiapiClient.CreateCompletionAsync(createCompletionRequest);
+                Assert.False(completionResponse.HasError, completionResponse?.ErrorResponse?.Error?.Message);
+                Assert.NotNull(completionResponse?.OpenAIResponse);
+                Assert.NotNull(completionResponse?.OpenAIResponse?.Choices);
+                Assert.True(completionResponse.OpenAIResponse.Choices.Count > 0);
+                Assert.NotNull(completionResponse?.OpenAIResponse?.Usage);
+
+            }
+        }
+
+
+        [Theory]
+        [InlineData("text-davinci-003")]
+        //[InlineData("gpt-3.5-turbo")]
         public async Task CreateCompletionStreamTest(string modelID)
         {
 
